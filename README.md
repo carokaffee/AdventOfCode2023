365f5d2a9a637727b687da234d23e0c8230de08b
carokaffee <carolin.kaffine@gmail.com>
2023-12-01 06:39:33 +0100

    day 1

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ My solutions for AoC 2023 written in Python.
 
 | Day :christmas_tree: | Problem | Solution | | Day :christmas_tree: | Problem | Solution |
 | :---: | --- | --- | --- | :---: | --- | --- |
-| 01 | [](https://adventofcode.com/2023/day/1) | [](src/solutions/day01.py) || 14 | [](https://adventofcode.com/2023/day/14) | [](src/solutions/day14.py) |
+| 01 | [Trebuchet?!](https://adventofcode.com/2023/day/1) | [day01.py](src/solutions/day01.py) || 14 | [](https://adventofcode.com/2023/day/14) | [](src/solutions/day14.py) |
 | 02 | [](https://adventofcode.com/2023/day/2) | [](src/solutions/day02.py) || 15 | [](https://adventofcode.com/2023/day/15) | [](src/solutions/day15.py) |
 | 03 | [](https://adventofcode.com/2023/day/3) | [](src/solutions/day03.py) || 16 | [](https://adventofcode.com/2023/day/16) | [](src/solutions/day16.py) |
 | 04 | [](https://adventofcode.com/2023/day/4) | [](src/solutions/day04.py) || 17 | [](https://adventofcode.com/2023/day/17) | [](src/solutions/day17.py) |
--- src/solutions/day01.py
+++ src/solutions/day01.py
@@ -1,7 +1,46 @@
 from src.tools.loader import load_data
 
-TESTING = True
+TESTING = False
+
+CONVERSION_TABLE = {
+    "one": "o1ne",
+    "two": "tw2o",
+    "three": "th3ree",
+    "four": "fo4ur",
+    "five": "fi5ve",
+    "six": "si6x",
+    "seven": "se7ven",
+    "eight": "eig8ht",
+    "nine": "ni9ne",
+    "zero": "ze0ro",
+}
+
+
+def convert_words_to_numbers(str):
+    for key in CONVERSION_TABLE.keys():
+        str = str.replace(key, CONVERSION_TABLE[key])
+    return str
+
+
+def find_calibration_value(str, calibration):
+    numbers = ""
+    if calibration:
+        str = convert_words_to_numbers(str)
+    for el in str:
+        if el in "0123456789":
+            numbers += el
+    return int(numbers[0] + numbers[-1])
 
 
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
+
+    # PART 1
+    # test:   -----
+    # answer: 55130
+    print(sum(list(map(lambda x: find_calibration_value(x, False), data))))
+
+    # PART 2
+    # test:     281
+    # answer: 54985
+    print(sum(list(map(lambda x: find_calibration_value(x, True), data))))
