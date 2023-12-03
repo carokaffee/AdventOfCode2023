fe7102c586cc06338533b2efb2202a92dc80d36c
carokaffee <carolin.kaffine@gmail.com>
2023-12-03 16:52:24 +0100

    day 2 cleanup

--- src/solutions/day02.py
+++ src/solutions/day02.py
@@ -1,52 +1,65 @@
 from src.tools.loader import load_data
+import math
 
 TESTING = False
+
+COLOURS = ("red", "green", "blue")
 
 
 def parse_input(data):
     games = []
     for line in data:
         id = int(line.split(":")[0].split(" ")[1])
-        games_str = list(line.split(": ")[1].split(";"))
-        takes = []
-        for game in games_str:
-            takes.append({"red": 0, "green": 0, "blue": 0})
-            colors = game.split(",")
-            for color in colors:
-                num, color_str = color.strip().split(" ")
-                num = int(num)
-                takes[-1][color_str] = num
-        games.append((id, takes))
+        reveal_strings = list(line.split(": ")[1].split(";"))
+        reveals = []
+        for reveal in reveal_strings:
+            reveals.append({colour: 0 for colour in COLOURS})
+            cubes = reveal.split(",")
+            for cube in cubes:
+                num, colour = cube.strip().split(" ")
+                reveals[-1][colour] = int(num)
+        games.append((id, reveals))
     return games
+
+
+def is_possible(game):
+    possible = True
+    for reveal in game:
+        if reveal["red"] > 12 or reveal["green"] > 13 or reveal["blue"] > 14:
+            possible = False
+    return possible
+
+
+def sum_of_possible_game_ids(games):
+    sum_ids = 0
+    for id, game in games:
+        if is_possible(game):
+            sum_ids += id
+    return sum_ids
+
+
+def sum_of_powers(games):
+    sum_powers = 0
+    for _, game in games:
+        min_required = {colour: 0 for colour in COLOURS}
+        for reveal in game:
+            for colour in COLOURS:
+                min_required[colour] = max(min_required[colour], reveal[colour])
+
+        sum_powers += math.prod([min_required[colour] for colour in COLOURS])
+    return sum_powers
 
 
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
     games = parse_input(data)
 
-    sum_ids = 0
-    for id, game in games:
-        possible = True
-        for take in game:
-            if take["red"] > 12 or take["blue"] > 14 or take["green"] > 13:
-                possible = False
+    # PART 1
+    # test:      8
+    # answer: 2512
+    print(sum_of_possible_game_ids(games))
 
-        if possible:
-            sum_ids += id
-
-    print(sum_ids)
-
-    sum_powers = 0
-    for id, game in games:
-        min_required = [0, 0, 0]
-        for take in game:
-            if take["red"] > min_required[0]:
-                min_required[0] = take["red"]
-            if take["blue"] > min_required[1]:
-                min_required[1] = take["blue"]
-            if take["green"] > min_required[2]:
-                min_required[2] = take["green"]
-        power = min_required[0] * min_required[1] * min_required[2]
-        sum_powers += power
-
-    print(sum_powers)
+    # PART 2
+    # test:    2286
+    # answer: 67335
+    print(sum_of_powers(games))
