189f1dc52dcd55aa0ae414d054c21f7fcbeec9ce
carokaffee <carolin.kaffine@gmail.com>
2023-12-07 11:29:56 +0100

    day 7 part 2 not working

--- src/solutions/day07.py
+++ src/solutions/day07.py
@@ -1,6 +1,6 @@
 from src.tools.loader import load_data
 
-# import itertools
+import itertools
 from collections import Counter
 
 TESTING = False
@@ -22,8 +22,9 @@ def parse_input(data):
 def sort_by_category(hands):
     categorised_hands = {category: [] for category in CATEGORIES}
     for hand in hands:
-        cards, bid = hand
-        counts = dict(Counter(cards))
+        cards, _ = hand
+        cropped_cards = cards[:5]
+        counts = dict(Counter(cropped_cards))
         vals = list(counts.values())
         if 5 in vals:
             categorised_hands["five"].append(hand)
@@ -46,21 +47,65 @@ def sort_by_highest_card(hands):
     return sorted(hands, key=lambda x: x[0])
 
 
+def sort_hands(hands):
+    hands = sort_by_category(hands)
+    sorted_hands = []
+    for category in CATEGORIES[::-1]:
+        sorted_hands += sort_by_highest_card(hands[category])
+    return sorted_hands
+
+
 def get_score(hands):
     res = 0
     for i, hand in enumerate(hands):
-        cards, bid = hand
+        _, bid = hand
         res += (i + 1) * bid
     return res
+
+
+def find_max_val_for_J(hand):
+    cards, bid = hand
+    num_J = cards.count(11)
+    if num_J == 0:
+        return cards + [0, 0, 0, 0, 0], bid
+    elif num_J == 1:
+        cards = list([i if i != 11 else 1 for i in cards])
+        return cards + [0, 0, 0, 0, 0], bid
+    else:
+        possible_J = list(
+            itertools.combinations([i for i in range(2, 15) if i != 11], num_J)
+        )
+        all_possible_hands = []
+        for J_tuple in possible_J:
+            counter = 0
+            new_hand = []
+            new_hand_joker = []
+            for item in cards:
+                if item == 11:
+                    new_hand.append(J_tuple[counter])
+                    new_hand_joker.append(-1)
+                    counter += 1
+                else:
+                    new_hand.append(item)
+                    new_hand_joker.append(0)
+            new_hand = (list(new_hand) + list(new_hand_joker), bid)
+            all_possible_hands.append(new_hand)
+        all_possible_hands = sort_hands(all_possible_hands)
+        return all_possible_hands[-1]
 
 
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
     hands = parse_input(data)
-    categorised_hands = sort_by_category(hands)
-
-    sorted_hands = []
-    for category in CATEGORIES[::-1]:
-        sorted_hands += sort_by_highest_card(categorised_hands[category])
+    sorted_hands = sort_hands(hands)
 
     print(get_score(sorted_hands))
+
+    new_best_hands = []
+    for hand in hands:
+        new_best_hands.append(find_max_val_for_J(hand))
+
+    new_best_hands = sort_hands(new_best_hands)
+    score = get_score(new_best_hands)
+
+    print(score)
