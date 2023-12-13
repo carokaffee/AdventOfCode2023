381ae47f483e194803d63c92acc2c01f0a700cf1
carokaffee <carolin.kaffine@gmail.com>
2023-12-13 07:35:03 +0100

    day 13 ugly

--- src/solutions/day13.py
+++ src/solutions/day13.py
@@ -1,6 +1,6 @@
 from src.tools.loader import load_data
 
-TESTING = True
+TESTING = False
 
 
 def parse_input(data):
@@ -36,7 +36,22 @@ def check_symmetry(block, i):
             symmetric = False
     return symmetric
 
+
 def check_smudge_symmetry(block, i):
+    num_iterations = min(i + 1, len(block) - i - 1)
+    symmetric = True
+    found_smudge = False
+    for j in range(num_iterations):
+        if [block[i - j][n] == block[i + 1 + j][n] for n in range(len(block[i]))].count(
+            False
+        ) == 1 and not found_smudge:
+            found_smudge = True
+        elif not block[i - j] == block[i + j + 1]:
+            # print("got here")
+            symmetric = False
+    if found_smudge == False:
+        return False
+    return symmetric
 
 
 if __name__ == "__main__":
@@ -48,29 +63,33 @@ if __name__ == "__main__":
     horizontal_sum = 0
     vertical_sum = 0
     for x in range(len(blocks)):
-        #print()
-        #print("NEW BLOCK", x)
-        #print()
+        # print()
+        # print("NEW BLOCK", x)
+        # print()
         block = blocks[x]
-        #print_block(block)
+        # print_block(block)
         found_match = False
         for i in range(len(block) - 1):
-            if block[i] == block[i + 1]:
-                if check_symmetry(block, i):
+            if [block[i][n] == block[i + 1][n] for n in range(len(block[i]))].count(
+                False
+            ) <= 1:
+                if check_smudge_symmetry(block, i):
                     horizontal_sum += 100 * (i + 1)
-                    #print("horizontal symmetry at", i + 1)
-                    #print("adding", 100 * (i + 1))
+                    # print("horizontal symmetry at", i + 1)
+                    # print("adding", 100 * (i + 1))
                     if found_match == True:
                         raise ValueError("already found symmetry")
                     found_match = True
         inv_block = blocks_inverted[x]
-        #print_block(inv_block)
+        # print_block(inv_block)
         for i in range(len(inv_block) - 1):
-            if inv_block[i] == inv_block[i + 1]:
-                if check_symmetry(inv_block, i):
+            if [
+                inv_block[i][n] == inv_block[i + 1][n] for n in range(len(inv_block[i]))
+            ].count(False) <= 1:
+                if check_smudge_symmetry(inv_block, i):
                     vertical_sum += i + 1
-                    #print("vertical symmetry at ", i + 1)
-                    #print("adding", i + 1)
+                    # print("vertical symmetry at ", i + 1)
+                    # print("adding", i + 1)
                     if found_match == True:
                         raise ValueError("already found symmetry")
                     found_match = True
